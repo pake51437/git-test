033abede5a4b361fa03766b3fc3e5bed7ee46503
pake51437 <1986643899@qq.com>
2021-09-28 18:44:46 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 卫天乐
+ * @Date: 2021/09/28/18:42
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
